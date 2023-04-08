8e13383f15dd497b1fd40978e2d15f6780cea1e9
Machado <andre.f.machado@cgi.com>
2023-04-08 20:56:34 +0100

    Miss quality something is wrong with the formula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,143 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'NineChroniclesItemStats';
+export class AppComponent implements OnInit {
+
+  private formGroupItem: FormGroup = new FormGroup({
+    typeOfItem: new FormControl(''),
+    typeOfElement: new FormControl('')
+  });
+
+  private formGroupRolls: FormGroup = new FormGroup({
+    atkSpell: new FormControl(null, Validators.required),
+    atkSpellValue: new FormControl(),
+    rolledAtk: new FormControl(null, Validators.required),
+    rolledSpd: new FormControl(null, Validators.required)
+  });
+
+  public get typeOfItem(): FormControl { return this.formGroupItem.get('typeOfItem') as FormControl; }
+  public get typeOfElement(): FormControl { return this.formGroupItem.get('typeOfElement') as FormControl; }
+
+  public get atkSpell(): FormControl { return this.formGroupRolls.get('atkSpell') as FormControl; }
+  public get atkSpellValue(): FormControl { return this.formGroupRolls.get('atkSpellValue') as FormControl; }
+  public get rolledAtk(): FormControl { return this.formGroupRolls.get('rolledAtk') as FormControl; }
+  public get rolledSpd(): FormControl { return this.formGroupRolls.get('rolledSpd') as FormControl; }
+
+
+  public typeOfItems = [
+    {
+      label: 'Weapons',
+      value: 1
+    },
+    {
+      label: 'Armor',
+      value: 2
+    },
+    {
+      label: 'Belt',
+      value: 3
+    },
+    {
+      label: 'Necklace',
+      value: 4
+    },
+    {
+      label: 'Ring',
+      value: 5
+    }
+  ];
+
+  public typeOfElements = [
+    {
+      label: 'Fire',
+      value: 'Fire'
+    },
+    {
+      label: 'Water',
+      value: 'Water'
+    },
+    {
+      label: 'Land',
+      value: 'Land'
+    },
+    {
+      label: 'Wind',
+      value: 'Wind'
+    },
+    {
+      label: 'Neutral',
+      value: 'Neutral'
+    }
+  ];
+
+  public weapons = [
+    {
+      name: 'WAR Sword',
+      type: 'Neutral',
+      baseAtk: 3094,
+      atkMin: 1099,
+      spdMin: 50.88,
+      atkMin2: 841,
+      atkMax: 1412,
+      spdMax: 64.57,
+      atkMax2: 1128,
+      image: "https://static.wikia.nocookie.net/gensin-impact/images/c/c9/Weapon_Traveler%27s_Handy_Sword.png/revision/latest?cb=20201116035456"
+    }
+  ];
+
+
+  public searchItems: any = [];
+  public selectedItem: any;
+  public rollStats: any;
+
+  public ngOnInit(): void {
+    this.typeOfElement.valueChanges.subscribe(value => {
+      if (this.typeOfItem.value === 1) {
+        this.searchItems = this.weapons.filter( weapon => weapon.type === value);
+      } else if (this.typeOfItem.value === 2) {
+
+      } else if (this.typeOfItem.value === 3) {
+
+      } else if (this.typeOfItem.value === 4) {
+
+      } else if (this.typeOfItem.value === 5) {
+
+      }
+
+    });
+  }
+
+  public checkQuality(item: any): void {
+    this.selectedItem = item;
+  }
+
+  public goBackToItems(): void {
+    this.selectedItem = null;
+    this.rollStats = null;
+  }
+
+  public checkResults(): void {
+    if (this.formGroupRolls.valid) {
+      this.rollStats = {
+        rollStrengthAtk: ((this.rolledAtk.value - (this.selectedItem.atkMin + this.selectedItem.atkMin2)) / ((this.selectedItem.atkMax + this.selectedItem.atkMax2) - (this.selectedItem.atkMin + this.selectedItem.atkMin2))),
+        rollStrengthSpd: ((this.rolledSpd.value - (this.selectedItem.spdMin)) / ((this.selectedItem.spdMax) - (this.selectedItem.spdMin))),
+        statStrengthAtk: (this.rolledAtk.value / (this.selectedItem.atkMax + this.selectedItem.atkMax2)),
+        statStrengthSpd: (this.rolledSpd.value / this.selectedItem.spdMax),
+        quality: 0
+      };
+      this.rollStats.quality = (((this.rollStats.rollStrengthAtk + this.rollStats.statStrengthAtk) / 2) + ((this.rollStats.rollStrengthSpd + this.rollStats.statStrengthSpd) / 2) / 2);
+    } else {
+      this.formGroupRolls.markAllAsTouched();
+    }
+  }
+
+
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
